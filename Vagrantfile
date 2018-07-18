cb03746cbd6da14786b56f2f8b3aafd1cf3f77c3
Mauricio Hernandez <Mauricio.Hernandez@endava.com>
2018-07-18 14:53:35 -0500

    ci-cd-challenge

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.provision "docker"
+  config.vm.provision "shell" do |s|
+    s.path = "provision.sh"
+  end
+  
+end
---
+++ provision.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+########################
+# Jenkins
+########################
+echo "Installing Java"
+sudo apt-get -y install default-jre > /dev/null 2>&1
+
+########################
+# Jenkins
+########################
+echo "Installing Jenkins"
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install jenkins > /dev/null 2>&1
+sudo usermod -aG docker jenkins
+########################
+# nginx
+########################
+echo "Installing nginx"
+sudo apt-get -y install nginx > /dev/null 2>&1
+sudo service nginx start
+
+########################
+# Configuring nginx
+########################
+echo "Configuring nginx"
+cd /etc/nginx/sites-available
+sudo rm default ../sites-enabled/default
+sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+sudo service nginx restart
+sudo service jenkins restart
+echo "Success"
